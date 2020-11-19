bad5bd6358ae4a6e350c8c3de16d0bdde743a6a0
iron6312 <edhardy312312@gmail.com>
2020-11-19 12:43:03 +0900

    マイグレーション名変更

--- db/schema.rb
+++ db/schema.rb
@@ -72,4 +72,5 @@ ActiveRecord::Schema.define(version: 2020_11_17_063749) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
